9b06ca87f31b9616e828b490379466ec2f3449b0
Shaminder Singh <spsingh94.ss@gmail.com>
2020-10-18 21:30:10 -0500

    narrowing down search from api to get app running efficiently

--- client/src/components/Generator/index.js
+++ client/src/components/Generator/index.js
@@ -4,73 +4,42 @@ import fetch from "node-fetch";
 
 function Generator() {
   const [apiResponse, setApiResponse] = useState();
-  const [selection, setSelection] = useState();
-  const [location, setLocation] = useState();
+  const [responseArray, setResponseArray] = useState(null);
+  const [selection, setSelection] = useState(null);
+  const [location, setLocation] = useState(null);
   //used to get random list from api
-  const [countryOffsetNumber, setCountryOffsetNumber] = useState();
-  const [cityOffsetNumber, setCityOffsetNumber] = useState();
+  const [number, setNumber] = useState();
+//   const [randomSelection, setRandomSelection] = useState();
   //   const [randomLocation, setRandomLocation] = useState();
 
   console.log(apiResponse);
   console.log(selection);
   console.log(location);
-  console.log(countryOffsetNumber);
-  console.log(cityOffsetNumber);
+  console.log(number);
+  console.log(responseArray);
+//   console.log(randomSelection);
   //   console.log(randomLocation);
 
   //   const selectionRef = useRef();
+
+  const apiKey = process.env.REACT_APP_RAPID_KEY;
 
   useEffect(() => {
     if (selection != null) {
       setLocation(selection);
     }
-  });
+  }, [selection]);
 
-  const apiKey = process.env.REACT_APP_RAPID_KEY;
-  //   offset=273685&limit=5
+  // offset=273685&limit=5
+
+  // /v1/geo/countries?offset=192&limit=5
 
   useEffect(() => {
-    if (location === "countries") {
-      // fetch(`https://rapidapi.p.rapidapi.com/v1/geo/${location}`,
-      fetch(
-        `https://rapidapi.p.rapidapi.com//v1/geo/${location}?offset=${countryOffsetNumber}&limit=5`,
-        {
-          method: "GET",
-          headers: {
-            "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-            "x-rapidapi-key": `${apiKey}`,
-          },
-        }
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          setApiResponse([data]);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-
+    if (location != null) {
       //   fetch(
-      //     "https://parseapi.back4app.com/classes/Continentscountriescities_Country?limit=10&keys=name",
-      //     {
-      //       headers: {
-      //         "X-Parse-Application-Id":
-      //           "Q8CoGDSW3oYZuGMWChYZHW5yff6Pa39dH6RXKdS8", // This is your app's application id
-      //         "X-Parse-REST-API-Key": "Av3j1sexUW7oV9kQCnbiAYgKvYxBcezfTf5w3REr", // This is your app's REST API key
-      //       },
-      //     }
-      //   )
-      //     .then((data) => {
-      //       setApiResponse([data]);
-      //     })
-      //     .catch((err) => {
-      //       console.error(err);
-      //     });
-    }
-    if (location === "cities") {
-      // fetch(`https://rapidapi.p.rapidapi.com/v1/geo/${location}`,
+      //     `https://rapidapi.p.rapidapi.com/v1/geo/${location}`,
       fetch(
-        `https://rapidapi.p.rapidapi.com//v1/geo/${location}?offset=${cityOffsetNumber}&limit=5`,
+        `https://rapidapi.p.rapidapi.com//v1/geo/${location}?offset=${number}&limit=5`,
         {
           method: "GET",
           headers: {
@@ -83,27 +52,35 @@ function Generator() {
         .then((response) => response.json())
         .then((data) => {
           setApiResponse([data]);
+          setLocation(null);
         })
         .catch((err) => {
           console.error(err);
         });
     }
     setSelection(null);
-    setLocation(null);
-  }, [location, apiKey, cityOffsetNumber, countryOffsetNumber]);
+    setApiResponse(null);
+  }, [apiKey, location, number]);
 
-  //   useEffect(() => {
-  //     if(apiResponse != null) {
-  //         setRandomLocation(apiResponse[0].data[50]);
-  //     }
-  // }, [apiResponse]);
+  useEffect(() => {
+    if (apiResponse != null) {
+      setResponseArray(apiResponse);
+    }
+    setApiResponse(null);
+  }, [apiResponse, responseArray]);
+
+//   useEffect(() => {
+//     if (responseArray != null) {
+//       setRandomSelection(responseArray[0].data[0].name);
+//     }
+//   }, [responseArray]);
 
   function selectedCity(e) {
     setSelection(e.currentTarget.value);
 
     //get random number for offset
     let x = Math.floor(Math.random() * 273684 + 1);
-    setCityOffsetNumber(x);
+    setNumber(x);
   }
 
   function selectedCountry(e) {
@@ -111,7 +88,7 @@ function Generator() {
 
     //get random number for offset
     let x = Math.floor(Math.random() * 194 + 1);
-    setCountryOffsetNumber(x);
+    setNumber(x);
   }
 
   return (
