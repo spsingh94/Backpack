c8d276b5b26c371880667309e6ba01df35e60491
Shaminder Singh <spsingh94.ss@gmail.com>
2020-10-19 20:27:42 -0500

    added current location

--- client/src/components/Generator/index.js
+++ client/src/components/Generator/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import fetch from "node-fetch";
 import { Maps } from "../Maps";
+// require('dotenv').config(path.resolve(process.cwd(), '.env'))
 
 function Generator() {
   const [apiResponse, setApiResponse] = useState();
@@ -8,10 +9,12 @@ function Generator() {
   const [selection, setSelection] = useState(null);
   const [location, setLocation] = useState(null);
   const [number, setNumber] = useState();
+  const [currentLocation, setCurrentLocation] = useState();
   const [mapLocation, setMapLocation] = useState();
   const [locationLat, setLocationLat] = useState(null);
   const [locationLong, setLocationLong] = useState(null);
   const [airportInfo, setAirportInfo] = useState();
+  const [airportCode, setAirportCode] = useState(null);
 
   // https://www.travelocity.com/Flights-Search?flight-type=on&starDate=11%2F18%2F2020&endDate=11%2F23%2F2020&_xpid=11905%7C1&mode=search&trip=roundtrip&leg1=from%3AChicago%2C+IL+%28ORD-O%27Hare+Intl.%29%2Cto%3AParis+%28PAR-All+Airports%29%2Cdeparture%3A11%2F18%2F2020TANYT&leg2=from%3AParis+%28PAR-All+Airports%29%2Cto%3AChicago%2C+IL+%28ORD-O%27Hare+Intl.%29%2Cdeparture%3A11%2F23%2F2020TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY
 
@@ -21,11 +24,20 @@ function Generator() {
   console.log(locationLat);
   console.log(locationLong);
   console.log(airportInfo);
+  console.log(currentLocation);
 
   const rapidKey = process.env.REACT_APP_RAPID_KEY;
   const googleKey = process.env.REACT_APP_GOOGLE_KEY;
 
   const mapsSource = `https://www.google.com/maps/embed/v1/place?key=${googleKey}&q=${mapLocation}`;
+
+  useEffect(() => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setCurrentLocation([position.coords.latitude, position.coords.longitude])
+        // console.log("Latitude is :", position.coords.latitude);
+        // console.log("Longitude is :", position.coords.longitude);
+      });
+  });
 
   useEffect(() => {
     if (selection != null) {
@@ -72,14 +84,6 @@ function Generator() {
     setResponseArray(null);
   }, [rapidKey, location, number, responseArray]);
 
-  function selectedCity(e) {
-    setSelection(e.currentTarget.value);
-
-    //get random number for offset
-    let x = Math.floor(Math.random() * 273684 + 1);
-    setNumber(x);
-  }
-
   useEffect(() => {
     if (locationLat != null && locationLong != null) {
       fetch(
@@ -88,26 +92,37 @@ function Generator() {
           method: "GET",
           headers: {
             "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
-            "x-rapidapi-key":
-            `${rapidKey}`,
+            "x-rapidapi-key": `${rapidKey}`,
           },
         }
       )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setAirportInfo(data.items);
-      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setAirportInfo(data.items);
+        })
         .catch((err) => {
           console.error(err);
         });
     }
   }, [rapidKey, locationLat, locationLong]);
 
+  useEffect(() => {
+    if (airportInfo != null) {
+      setAirportCode(airportInfo[0].iata);
+    }
+  }, [airportInfo]);
+
+  // function that gets random city
+  function selectedCity(e) {
+    setSelection(e.currentTarget.value);
+    let x = Math.floor(Math.random() * 273684 + 1);
+    setNumber(x);
+  }
+
+  // function that gets random country
   function selectedCountry(e) {
     setSelection(e.currentTarget.value);
-
-    //get random number for offset
     let x = Math.floor(Math.random() * 194 + 1);
     setNumber(x);
   }
@@ -133,10 +148,18 @@ function Generator() {
       <label for="country">Country</label>
       <br></br>
       <br />
-      <button>Search</button>
       <h1>{mapLocation}</h1>
 
       <Maps src={mapsSource} />
+      <button
+        onClick={() => {
+          window.open(
+            `https://www.skyscanner.com/transport/flights/mke/${airportCode}/201024/201031/?adults=1&adultsv2=1&cabinclass=economy&children=0&childrenv2=&destinationentityid=46516321&destinationgsid=46516321&inboundaltsenabled=false&infants=0&originentityid=27544948&outboundaltsenabled=false&preferdirects=false&preferflexible=false&ref=home&rtn=1`
+          );
+        }}
+      >
+        Redirect
+      </button>
     </>
   );
 }
