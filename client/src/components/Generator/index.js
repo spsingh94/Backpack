dfe56b37679c04af068184aa7f9c56cd84c1b3d7
Shaminder Singh <spsingh94.ss@gmail.com>
2020-10-19 19:46:27 -0500

    getting response from aeroDataBox API

--- client/src/components/Generator/index.js
+++ client/src/components/Generator/index.js
@@ -9,10 +9,18 @@ function Generator() {
   const [location, setLocation] = useState(null);
   const [number, setNumber] = useState();
   const [mapLocation, setMapLocation] = useState();
+  const [locationLat, setLocationLat] = useState(null);
+  const [locationLong, setLocationLong] = useState(null);
+  const [airportInfo, setAirportInfo] = useState();
+
+  // https://www.travelocity.com/Flights-Search?flight-type=on&starDate=11%2F18%2F2020&endDate=11%2F23%2F2020&_xpid=11905%7C1&mode=search&trip=roundtrip&leg1=from%3AChicago%2C+IL+%28ORD-O%27Hare+Intl.%29%2Cto%3AParis+%28PAR-All+Airports%29%2Cdeparture%3A11%2F18%2F2020TANYT&leg2=from%3AParis+%28PAR-All+Airports%29%2Cto%3AChicago%2C+IL+%28ORD-O%27Hare+Intl.%29%2Cdeparture%3A11%2F23%2F2020TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY
 
   console.log(apiResponse);
   console.log(responseArray);
   console.log(mapLocation);
+  console.log(locationLat);
+  console.log(locationLong);
+  console.log(airportInfo);
 
   const rapidKey = process.env.REACT_APP_RAPID_KEY;
   const googleKey = process.env.REACT_APP_GOOGLE_KEY;
@@ -51,6 +59,8 @@ function Generator() {
                 "," +
                 responseArray[0].data[0].name
             );
+            setLocationLat(responseArray[0].data[0].latitude);
+            setLocationLong(responseArray[0].data[0].longitude);
           }
         })
         .catch((err) => {
@@ -69,6 +79,30 @@ function Generator() {
     let x = Math.floor(Math.random() * 273684 + 1);
     setNumber(x);
   }
+
+  useEffect(() => {
+    if (locationLat != null && locationLong != null) {
+      fetch(
+        `https://rapidapi.p.rapidapi.com/airports/search/location/${locationLat}/${locationLong}/km/100/5?withFlightInfoOnly=false`,
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+            "x-rapidapi-key":
+            `${rapidKey}`,
+          },
+        }
+      )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAirportInfo(data.items);
+      })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [rapidKey, locationLat, locationLong]);
 
   function selectedCountry(e) {
     setSelection(e.currentTarget.value);
@@ -100,6 +134,7 @@ function Generator() {
       <br></br>
       <br />
       <button>Search</button>
+      <h1>{mapLocation}</h1>
 
       <Maps src={mapsSource} />
     </>
