73036f6dbf03b7f970048f259eec2ddd61e40920
Shaminder Singh <spsingh94.ss@gmail.com>
2020-10-25 15:50:57 -0500

    using cors in server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
