417bbd0b4ffe8569f3aa7ef222a04f531c576941
Shaminder Singh <spsingh94.ss@gmail.com>
2020-10-25 22:46:28 -0500

    added access control code

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
