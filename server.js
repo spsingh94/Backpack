57e391b557c74dec6bd1fe0d851d108b743ac4fb
Shaminder Singh <spsingh94.ss@gmail.com>
2020-10-28 03:04:28 -0500

    updated server

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ app.use("/", router);
 // Serve up static assets (usually on heroku)
   app.use(express.static("client/build"));
 
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "client/public/index.html"), function (
-//     err
-//   ) {
-//     if (err) {
-//       res.status(500).send(err);
-//     }
-//   });
-// });
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/public/index.html"), function (
+    err
+  ) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 
 // Start the API server
 app.listen(PORT, function () {
